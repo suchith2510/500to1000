47d2832e5a67b0df25390f8de7930fec43a15b6d
suchith <suchithpuppala@gmail.com>
2025-08-14 22:33:12 +0530

    codechef 8

---
+++ Qualify.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Qualify
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int x=sc.nextInt();
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            if(a*1+b*2>=x){
+                System.out.println("Qualify");
+            }
+            else{
+                System.out.println("NotQualify");
+            }
+        }
+	}
+}
