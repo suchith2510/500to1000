a975b6b984dacf7aadfe4ad0cac4694032c14146
suchith <suchithpuppala@gmail.com>
2025-08-14 22:34:50 +0530

    codechef 8

--- Qualify.java
+++ Qualify.java
@@ -6,7 +6,6 @@ class Qualify
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
         while(t-->0){
